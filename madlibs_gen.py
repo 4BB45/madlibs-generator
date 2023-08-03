e29fb9fdeeaa8558719fdc9e8429b22b305e61d7
abbas <136506470+4BB45@users.noreply.github.com>
2023-08-03 20:27:01 +0100

    Add files via upload

---
+++ madlibs_gen.py
@@ -0,0 +1,6 @@
+madlib1 = input('input an adjective: ')
+madlib2 = input('input a verb: ')
+madlib3 = input('input a noun: ')
+
+print(f'The {madlib1}, {madlib2} {madlib3}')
+print('what the fuck is wrong with you')
